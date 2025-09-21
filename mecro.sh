ab096fb0071bbd049cee9e55d8dd8f16c2c73cc8
janggun-jeon <man88821@gmail.com>
2025-09-21 21:41:04 +0900

    2025-09-21 Upload extra-data

---
+++ mecro.sh
@@ -0,0 +1,31 @@
+# ! /bin/sh 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/1.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/2.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/3.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/4.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/5.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/6.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/7.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/8.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/9.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/10.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/11.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/12.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/13.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/14.out 2>&1 
+nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/15.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/16.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/17.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/18.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/19.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/20.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/21.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/22.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/23.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/24.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/25.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/26.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/27.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/28.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/29.out 2>&1 
+# nohup python train.py --data amazon --embed_rescalied --scale_regularization layer_norm > ./log/amazon/seed-72/log/30.out 2>&1 
